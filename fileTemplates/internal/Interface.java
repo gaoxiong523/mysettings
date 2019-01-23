008ea1b34107ade02e67b0df6d657e084e036ccf
200506725@qq.com <gaojunchen520>
2019-01-23 22:34:54 +0800

    IU-2018.1.3 <Administrator@PC-20170621WOWM Create markdown.xml, p3c.xml, IntelliLang.xml, print.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Interface.java, ActionScript.xml, yiiguxing.translation.xml, GSP.xml, Default.xml, AngularJS .xml, ignore.xml, Nice Python.icls, find.xml, keymap.xml, gradle.run.settings.xml, AndroidComments.xml, AndroidLog.xml, JSP.xml, Web Services.xml, _@user_Darcula.icls, Zen HTML.xml, mavenVersion.xml, Default.xml, RESTful Web Services.xml, JavaScript.xml, debugger.renderers.xml, AndroidParcelable.xml, Eclipse copy.xml, Kotlin.xml, Zen XSL.xml, plain.xml, editor.codeinsight.xml, grepConsole.xml, Android.xml, iterations.xml, OGNL.xml, editor.xml, debugger.xml, ide.general.xml, git.xml, CodeGlance.xml, rainbow_brackets.xml, lombok-plugin.xml, other.xml, Zen CSS.xml, JAVA.xml, androidStudioFirstRun.xml, ui.lnf.xml, gradle.settings.xml, log_highlighting.xml, filetypes.xml, php.xml, Solarized Light.xml, javaeeExternalResources.xml, xsl.xml, plugin_ui.xml, JavaScript Testing.xml, colors.scheme.xml, html_xml.xml, alibaba-toolkit-settings.xml, databaseDrivers.xml, templates.xml, Groovy.xml, output.xml, Angular.xml, OGNL _Struts 2_.xml, laf.xml, parameter.hints.xml, AndroidXML.xml, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, ColdFusion.xml, proxy.settings.xml, HTTP Request.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, nbmindmapsettings.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ *@ClassName ${NAME}
+ *@Description TODO
+ *@author gaoxiong
+ *@date ${DATE} ${TIME}
+ */
+public interface ${NAME} {
+}
